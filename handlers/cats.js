7ecaf28425695689f2828326b95644488c2293d8
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-05-24 15:58:07 +0300

    tweaks

--- handlers/cats.js
+++ handlers/cats.js
@@ -3,35 +3,31 @@ const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
 const formidable = require('formidable');
-// const cats = require('../data/cats.json');
-// const breeds = require('../data/breeds.json');
-
-const getHandler = (res, req, view) => {
-    const filePath = path.normalize(
-        path.join(__dirname, `../views/cats/${view}.html`));
-
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.write('File not found.');
-            res.end();
-            return
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
-    });
-}
-
+const cats = require('../data/cats.json');
+const breeds = require('../data/breeds.json');
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     //BREED
     if (pathname === '/cats/addBreed' && req.method === 'GET') {
-        getHandler(res, req, 'addBreed');
+        const filePath = path.normalize(
+            path.join(__dirname, `../views/cats/addBreed.html`));
+
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('File not found.');
+                res.end();
+                return
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+
     } else if (pathname === '/cats/addBreed' && req.method === 'POST') {
         let body = '';
         req.on('data', function (data) {
@@ -55,17 +51,45 @@ module.exports = (req, res) => {
                     res.end();
                     return
                 }
-                const breeds = JSON.parse(data);
-                breeds.push(post.breed);
+                const tmp = JSON.parse(data);
+                tmp.push(post.breed);
 
-                fs.writeFileSync(filePath, JSON.stringify(breeds));
+                fs.writeFileSync(filePath, JSON.stringify(tmp));
+                res.writeHead(301, { "Location": "/" });
+                return res.end();
             });
         });
     }
 
     //CATS
     if (pathname === '/cats/addCat' && req.method === 'GET') {
-        getHandler(res, req, 'addCat');
+        const filePath = path.normalize(
+            path.join(__dirname, `../views/cats/addCat.html`));
+
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('File not found.');
+                res.end();
+                return
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+
+            console.log(breeds);
+            const breedOptions = breeds.map(breed => {
+                return `<option value="${breed}">${breed}</option>`
+            });
+
+            console.log(breedOptions);
+            const modifiedData = data.toString()
+                .replace('{{catBreeds}}', breedOptions);
+
+            res.write(modifiedData);
+            res.end();
+        });
+
     } else if (pathname === '/cats/addCat' && req.method === 'POST') {
         let body = '';
         req.on('data', function (data) {
@@ -101,7 +125,10 @@ module.exports = (req, res) => {
                 );
 
                 fs.writeFileSync(filePath, JSON.stringify(tmp));
+                res.writeHead(301, { "Location": "/" });
+                return res.end();
             });
         });
     }
+
 }
